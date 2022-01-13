5fe72a1bbeb85881c11c8f67a6a846f535c97ad9
Salamat <salamat@mail.ru>
2022-01-13 17:06:46 +0500

    2 lesson is over

--- js/main.js
+++ js/main.js
@@ -1,36 +1,39 @@
-const btns = document.querySelectorAll('button'),
-      wrapper = document.querySelector('.btn-block');
+window.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
 
-// console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(1));
-// console.log(btns[1].classList.add('red', 'qwer'));
-// console.log(btns[0].classList.remove('blue'));
-// console.log(btns[0].classList.toggle('blue'));
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
 
-// if(btns[1].classList.contains('red')) {
-//     console.log('red');
-// } 
-
-
-btns[0].addEventListener('click', () => {
-    // if (!btns[1].classList.contains('red')) {
-    //     btns[1].classList.add('red');
-    // } else {
-    //     btns[1].classList.remove('red');
-    // }
-
-    btns[1].classList.toggle('red');
-});
-
-// console.log(btns[0].className);
-
-
-wrapper.addEventListener('click', (e) => {
-    if(e.target && e.target.tagName == 'BUTTON') {
-        console.log('Hello');
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
     }
-});
 
-const btn = document.createElement('button');
-btn.classList.add('red');
-wrapper.append(btn);
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if(target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
