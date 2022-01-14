c5a0d479e47492ee3515fd55f668f6b7138455a1
Salamat <salamat@mail.ru>
2022-01-14 15:43:06 +0500

    4 lesson is over

--- js/main.js
+++ js/main.js
@@ -1,35 +1,27 @@
-const btn = document.querySelector('.btn');
-let timerId;
-let i = 0;
+'use strict';
 
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
+const now = new Date('2022-03-01');
+    // new Date.parse('2022-05-01');
 
-    const id = setInterval(frame, 10);
+console.log(now.setHours(20));
+console.log(now);
 
-    function frame() {
-        if(pos === 300) {
-            clearInterval();
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
-    }
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+
+let start = new Date();
+
+for(let i = 0; i < 100000; i++) {
+    let some = i ** 3;
 }
 
-btn.addEventListener('click', myAnimation);
+let end = new Date();
 
-// function logger() {
-//     if(i === 3) {
-//         clearInterval(timerId);
-//     }
-//     console.log('text');
-//     i++;
-// }
-
-// let id = setTimeout(function log() {
-//     console.log('Hello');
-//     id = setTimeout(log, 500);
-// }, 500);
+alert(`Цикл отработал за ${end - start} миллисекунд`);
