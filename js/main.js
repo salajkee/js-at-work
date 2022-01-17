4cb7408f8b96d3038f66524f95e4590b7e4fad94
Salamat <salamat@mail.ru>
2022-01-17 13:57:49 +0500

    10 lesson (this) is over

--- js/main.js
+++ js/main.js
@@ -1,27 +1,87 @@
 "use strict";
 
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log(`Hello ${this.name}`);
-    };
-}
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+
+//     console.log(sum());
+// }
+
+// showThis(4, 5);
 
 
-User.prototype.exit = function() {
-    console.log(`Пользователь ${this.name} ушел`);
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this);
+//     }
+// };
+// obj.sum();
+
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log('Hello! ' + this.name);
+//     };
+// }
+
+// let ivan = new User('Ivan', 23);
+
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// const  user = {
+//     name: 'John'
+// };
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(13));
+
+// 1) Обычная функция: this = window, но если use strict - undefined
+// 2) Контекст у методов объекта - сам объект
+// 3) this в конструкторах и классах - это новый экземпляр объекта
+// 4) Ручная привязка this: call, apply, bind
+
+
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', (e) => {
+     e.target.style.background = 'red';
+});
+
+const obj = {
+    num: 5,
+    sayNumber: function() {
+        const say = () => {
+            console.log(this.num);
+        };
+
+        say();
+    }
 };
 
+obj.sayNumber();
 
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 20);
 
-ivan.exit();
+const double = a => a*2;
 
-ivan.hello();
-alex.hello();
-
-console.log(ivan);
-console.log(alex);
+console.log(double(8));
